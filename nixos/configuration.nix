b79d096540bea5196fe7b1011848f13fec1311a5
vermilion10 <180294769+vermilion10@users.noreply.github.com>
2025-06-30 21:27:27 +0700

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -14,13 +14,13 @@
     ];
 
   # Bootloader.
-  #boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   #boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.grub.enable = true;
-  boot.loader.grub.devices = [ "nodev" ];
-  boot.loader.grub.efiSupport = true;
-  boot.loader.grub.useOSProber = true;
+#  boot.loader.grub.enable = true;
+#  boot.loader.grub.devices = [ "nodev" ];
+#  boot.loader.grub.efiSupport = true;
+#  boot.loader.grub.useOSProber = true;
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
